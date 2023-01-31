6342cf312aacdeee064bd521a0484105a2ce47b5
Grace Lee <gracelee9901@gmail.com>
2023-01-30 19:52:13 -0800

    Create Animal and ApiHelper models

---
+++ CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CretaceousClient.Models;
+
+#nullable enable
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
